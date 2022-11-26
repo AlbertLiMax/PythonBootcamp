1262bfee32096fd2d0a1f704a1a9b7b15c076bfc
albertmax <matrix.ee91g@gmail.com>
2022-11-26 14:21:37 +0800

    day-18-5

--- day-18-5-draw-a-spirograph/main.py
+++ day-18-5-draw-a-spirograph/main.py
@@ -2,7 +2,10 @@ import turtle as t
 import random
 
 tim = t.Turtle()
+tim.speed(0)
 t.colormode(255)
+
+
 def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
@@ -13,4 +16,10 @@ def random_color():
 ########### Challenge 5 - Spirograph ########
 
 
+for i in range(0, 60):
+    tim.color(random_color())
+    tim.circle(200)
+    tim.setheading(i * 6)
 
+screen = t.Screen()
+screen.exitonclick()
