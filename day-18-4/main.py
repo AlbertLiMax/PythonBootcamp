5bfdc84a9abde5aa2c9bbc2899906aa464690a72
albertmax <matrix.ee91g@gmail.com>
2022-11-26 13:30:54 +0800

    day-18-4

--- day-18-4/main.py
+++ day-18-4/main.py
@@ -1,7 +1,18 @@
 import turtle as t
-import random
+import random as r
+from turtle import Screen
 
 tim = t.Turtle()
-
+tim.pensize(20)
 ########### Challenge 4 - Random Walk ########
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+colours = ["CornflowerBlue", "Yellow", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+angle = [90, 180, 270, 360]
+screen = Screen()
+tim.speed(0)
+
+for i in range(300):
+    tim.color(r.choice(colours))
+    tim.forward(30)
+    tim.setheading(r.choice(angle))
+
+screen.exitonclick()
