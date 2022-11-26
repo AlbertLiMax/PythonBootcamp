f8f8fbb3ab70f17a790b7782dc710c855bebea17
albertmax <matrix.ee91g@gmail.com>
2022-11-26 12:13:58 +0800

    day-18-2

--- day-18-2/main.py
+++ day-18-2/main.py
@@ -1,5 +1,23 @@
 import turtle as t
+from turtle import Screen as s
 
 tim = t.Turtle()
 
 ########### Challenge 2 - Draw a Dashed Line ########
+def dashed_line(length):
+    tim.color("black")
+    while length:
+        tim.pendown()
+        tim.forward(10)
+        length -= 10
+        print(length)
+        if length:
+            tim.penup()
+            tim.forward(10)
+            length -= 10
+            print(length)
+
+dashed_line(500)
+
+screen = s()
+screen.exitonclick()
