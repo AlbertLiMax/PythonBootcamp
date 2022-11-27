994bb14f213e720ed946e4abf9115e8159f6aacd
albertmax <matrix.ee91g@gmail.com>
2022-11-27 12:03:06 +0800

    day-19-turtle-race

--- day-20-turtle-race/main.py
+++ day-20-turtle-race/main.py
@@ -1,7 +1,33 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+turtles = []
 
+for i in range(len(colors)):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[i])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=(80 - i * 30))
+    turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+        if turtle.xcor() >= 250:
+            if user_bet == turtle.pencolor():
+                print(f"You've won. The {turtle.pencolor()} turtle is the winner!")
+            else:
+                print(f"You've lost. The {turtle.pencolor()} turtle is the winner!")
+            is_race_on = False
+            break
 
 screen.exitonclick()
