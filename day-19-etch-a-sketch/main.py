1c1bc272a505d138beb1627e8741db6d54169ee9
albertmax <matrix.ee91g@gmail.com>
2022-11-27 10:58:27 +0800

    day-19-etch-a-sketch

--- day-19-etch-a-sketch/main.py
+++ day-19-etch-a-sketch/main.py
@@ -7,20 +7,25 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+
 def move_backwards():
     tim.backward(10)
+
 
 def turn_left():
     tim.left(5)
 
+
 def turn_right():
     tim.right(5)
+
 
 def clear():
     tim.clear()
     tim.penup()
     tim.home()
     tim.pendown()
+
 
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
