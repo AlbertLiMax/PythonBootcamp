9110218d32cf70bd3ac27e9dd7a8fa7d9916b22c
albertmax <matrix.ee91g@gmail.com>
2022-12-27 14:48:34 +0800

    day-32-birthday-wisher

--- day-32-birthday-wisher/main.py
+++ day-32-birthday-wisher/main.py
@@ -1,13 +1,48 @@
 ##################### Extra Hard Starting Project ######################
+import smtplib
+import datetime as dt
+from random import choice
+import pandas
 
-# 1. Update the birthdays.csv
+# ---------------------------- LOAD DATABASE ----------------------------------------- #
+df = pandas.read_csv("birthdays.csv")
+database = df.to_dict(orient="records")
 
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
+letters = []
+for i in range(1, 4):
+    with open(f"letter_templates/letter_{i}.txt") as file:
+        letters.append(file.read())
 
 
+# ---------------------------- PREPARE LETTER ---------------------------------------- #
+def prepare_letter(name):
+    letter = choice(letters).replace("[NAME]", name)
+    return letter
 
+
+# ---------------------------- SEND LETTERS ------------------------------------------ #
+EMAIL = "alissa.cpr@gmail.com"
+PASSWORD = "placeholder"
+
+def send_letter(addr, title, msg):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL, to_addrs=f"{addr}", msg=f"Subject:{title}\n\n{msg}")
+
+
+# ---------------------------- CHECK IF BIRTHDAY MATCH ------------------------------- #
+def is_birthday(date):
+    today = dt.datetime.now()
+    if date.month == today.month and date.day == today.day:
+        return True
+    else:
+        return False
+
+
+# ---------------------------- LOOP THROUGH DATABASE --------------------------------- #
+for item in database:
+    date = dt.datetime(year=item["year"], month=item["month"], day=item["day"])
+    if is_birthday(date):
+        letter = prepare_letter(item["name"])
+        send_letter(item["email"], "Happy Birthday!", letter)
