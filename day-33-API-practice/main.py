c2d1cd6aa5d35432cf554e0dbfaadd605cef3041
albertmax <matrix.ee91g@gmail.com>
2023-01-01 20:57:54 +0800

    day-33-API

--- day-33-API-practice/main.py
+++ day-33-API-practice/main.py
@@ -1,13 +1,33 @@
 import requests
+from datetime import datetime, timezone
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# Raise exception if there are error status codes
+MY_LAT = 24.83
+MY_LONG = 121.01
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # Raise exception if there are error status codes
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 
 data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise, sunset)
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (longitude, latitude)
-print(iss_position)
+time_now = datetime.now(timezone.utc)
+print(time_now.hour)
